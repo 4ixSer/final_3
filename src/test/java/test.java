f5d80b5e4299a23be6c7bdf022689be313d49286
qny4i <gny4ix08@gamail.com>
2016-11-09 17:46:15 +0200

    Добавил астивную сесию и чуто правил юзера

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -3,6 +3,7 @@ import com.entity.District;
 import com.entity.Sauna;
 import com.sun.jnlp.ApiDialog;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,12 +15,20 @@ public class test {
 
     public static void main(String[] args) {
 
-
+//        System.out.println("SELECt a from Sauna a  where price >=" +10 +" and price<=" +12+"\'");
 
         Factoty.getInstance().getPhoneDAO();
 
-        Sauna sauna1 = Factoty.getInstance().getSaunaDAO().findSauna(1);
-        List<Sauna> saunas = Factoty.getInstance().getSaunaDAO().findByName("Сауна");
+        List<String> newList = new ArrayList<>();
+        newList.add("151");
+        newList.add("198");
+        newList.add("10");
+        newList.add("15");
+
+
+//
+//
+        List<Sauna> saunas = Factoty.getInstance().getSaunaDAO().findByCapacity(10,15);
 
         for (int i = 0; i <saunas.size() ; i++) {
             System.out.print("Название - "+saunas.get(i).getName());
@@ -32,7 +41,29 @@ public class test {
             System.out.println();
         }
 
+        List<Sauna> saunas1 = Factoty.getInstance().getSaunaDAO().findByPrice(151,198);
+        for (int i = 0; i <saunas1.size() ; i++) {
+            System.out.print("Название - "+saunas1.get(i).getName());
+            System.out.print(", цена = "+ saunas1.get(i).getPrice());
+            System.out.print(", вместимость = "+saunas1.get(i).getCapacity());
+            System.out.println(", расположенны на = "+ saunas1.get(i).getAddress());
+            System.out.println(saunas1.get(i).getDistrict());
+            System.out.println(saunas1.get(i).getType());
+            System.out.println(saunas1.get(i).getPhones());
+            System.out.println();
+        }
 
+        List<Sauna> saunas2 = Factoty.getInstance().getSaunaDAO().findByAll(newList);
+        for (int i = 0; i <saunas2.size() ; i++) {
+            System.out.print("Название - "+saunas2.get(i).getName());
+            System.out.print(", цена = "+ saunas2.get(i).getPrice());
+            System.out.print(", вместимость = "+saunas2.get(i).getCapacity());
+            System.out.println(", расположенны на = "+ saunas2.get(i).getAddress());
+            System.out.println(saunas2.get(i).getDistrict());
+            System.out.println(saunas2.get(i).getType());
+            System.out.println(saunas2.get(i).getPhones());
+            System.out.println();
+        }
 
 
 //        Factoty.getInstance().getSaunaDAO().changeSaunaName(1, "«Купель здоровья»");
